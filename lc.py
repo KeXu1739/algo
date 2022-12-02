92cd30173a34a5f2bd54402ed544a080b13ba99d
kexu17 <xuke173991@gmail.com>
2022-12-01 22:55:23 -0600

    20221201 lc

--- lc.py
+++ lc.py
@@ -1,5 +1,5 @@
 from algoHelper import *
-
+import numpy as np
 
 def lc_0001():
     # 2和
@@ -56,6 +56,42 @@ def lc_0027():
     print(removeElement(nums, t))
     print(nums)
 
+def lc_0035():
+    def compare(arr, target):
+        if target > arr[-1]:
+            return len(arr)
+        if target < arr[0]:
+            return 0
+        for i, ele in enumerate(arr):
+            if ele >= target:
+                return i
+    def searchInsert(nums: List[int], target: int) -> int:
+        # 找在有序数组中target的位置或者插入target的位置
+        # 使用对数器检查出这个二分搜索不适合有重复值的arr
+        if target > nums[-1]:
+            return len(nums)
+        if target < nums[0]:
+            return 0
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + ((r-l) >> 1)
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m
+        return r
+
+    for i in range(10000):
+        n = random.randint(1, 10000)
+        nums = sorted(list(set(list(np.random.randint(-10000, 10000, n)))))
+        target = random.randint(-10000, 10000)
+
+        if searchInsert(nums, target) != compare(nums, target):
+            print((nums, target))
+            break
+
 def lc_0066():
     def plusOne(digits: List[int]) -> List[int]:
         n = len(digits)
@@ -72,8 +108,90 @@ def lc_0066():
             digits.insert(0, 1)
         return digits
 
-    digits = [9,8,7,6,5,4,3,2,1,0]
-    print(plusOne(digits))
+    def plusOneOptimized(digits):
+        n = len(digits)
+        for i in range(n-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        digits.append(0)
+        digits[0] = 1
+        return digits
+
+
+    digits = [8,9,9,9,9,9]
+    print(plusOneOptimized(digits))
+
+def lc_0088():
+    def compare(nums1, nums2, m, n):
+        nums1[m:m+n] = nums2
+        return sorted(nums1)
+
+    def merge(nums1, m, nums2, n):
+        # 给两个有序数组inplace merge成一个有序数组，其中nums1数组末尾处刚开始时有占位元素
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        k = len(nums1)-1
+        p1, p2 = m-1, n-1
+        while k >= 0:
+            while p1 >= 0 and p2 >= 0:
+                if nums1[p1] > nums2[p2]:
+                    nums1[k] = nums1[p1]
+                    p1 -= 1
+                else:
+                    nums1[k] = nums2[p2]
+                    p2 -= 1
+                k -= 1
+            while p1 >= 0:
+                nums1[k] = nums1[p1]
+                p1 -= 1
+                k -= 1
+            while p2 >= 0:
+                nums1[k] = nums2[p2]
+                p2 -= 1
+                k -= 1
+        return
+
+    m = random.randint(1,200)
+    n = random.randint(1,200)
+    nums1 = sorted(list(np.random.randint(-1e9, 1e9, m))) + [0 for i in range(n)]
+    nums2 = sorted(list(np.random.randint(-1e9, 1e9, n)))
+    for i in range(50000):
+        A = compare(nums1, nums2, m, n)
+        merge(nums1, m, nums2, n)
+        B = nums1
+        if not all([a == b for a,b in zip(A, B)]):
+            print(nums1)
+            print(nums2)
+            print(m)
+            print(n)
+            break
+
+def lc_1704():
+    def halvesAreAlike(s: str) -> bool:
+        # 检查一个string左半边和右半边字符串元音字母数量是不是相等
+        vowels = {'a','e','i','o','u','A','E','I','O','U'}
+        l, r = 0, len(s) - 1
+        lvo, rvo = 0, 0
+        while l < r:
+            if s[l] in vowels:
+                lvo += 1
+            if s[r] in vowels:
+                rvo += 1
+            l += 1
+            r -= 1
+        return lvo == rvo
+    st = "halves"
+    print(st)
+    print(halvesAreAlike(st))
+
 
 if __name__ == "__main__":
-    lc_0066()
+    lc_0088()
